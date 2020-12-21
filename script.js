63c083b7511fe1e12d69f8b822ac785bafdaa336
Isaac Rayo <ar_bustupapi@hotmail.com>
2020-12-21 02:14:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+
+
+var zoom = function(){
+
+
+
+    let thumb = document.getElementById("thumb");
+
+    
+
+    if(thumb.clasName = "thumb grande"){
+        thumb.className = "thumb";
+    }else {
+        thumb.className = "thumb grande";
+    }
+    
+
+    
+
+        
+
+   /* if (thum.className == "thumb"){
+        thum.setAttribute("class", "thum grande");
+
+    
+        
+    }else{
+        thum.className == "thum";
+    }
+    */
+
+}   
+
+var zoom2 = function(){
+
+
+
+    let thumb = document.getElementById("thumb");
+
+    let clase = thumb.className;
+
+    //alert("la clase es "+clase);
+
+    if(clase == "thumb"){
+        thumb.className = "thumb grande";
+    }else{
+        thumb.className = "thumb";
+    }
+    
+
+    
+
+        
+
+   /* if (thum.className == "thumb"){
+        thum.setAttribute("class", "thum grande");
+
+    
+        
+    }else{
+        thum.className == "thum";
+    }
+    */
+
+}
